24ed7de99808059ef9d61196670f178a04b83b9e
Mahadi Hassan <mahadi.hassan@northsouth.edu>
2020-08-19 13:19:48 +0600

    a hooks added for file upload to firebase

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -11,18 +11,14 @@ const UploadForm = () => {
     const changeHandler = (e) => {
         // e.preventDefault();
         let selected = e.target.files[0];
-
-
         // console.log(selected);
         if (selected && types.includes(selected.type)) {
             setfile(selected);
             seterror('');
-
         } else {
             setfile(null);
             seterror('Please Select and image file (png or jpeg)');
         }
-
     }
     return (
         <form>
